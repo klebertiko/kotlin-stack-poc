4ae0fafac804f861f866b60ed9230ea9cbebca79
Kleber Almeida Toyota <klebertiko@gmail.com>
2019-03-08 16:25:49 -0300

    Added DAO and route to ItemController and function to ItemRepository

--- src/main/kotlin/org/kat/kotlinwebstack/application/web/item/ItemController.kt
+++ src/main/kotlin/org/kat/kotlinwebstack/application/web/item/ItemController.kt
@@ -3,8 +3,14 @@ package org.kat.kotlinwebstack.application.web.item
 import io.javalin.Context
 import org.kat.kotlinwebstack.domain.item.Item
 import org.kat.kotlinwebstack.domain.item.items
+import org.kat.kotlinwebstack.resources.persistence.ItemRepository
+import org.kat.kotlinwebstack.resources.utils.toJsonObject
+import org.koin.standalone.KoinComponent
+import org.koin.standalone.inject
 
-class ItemController {
+class ItemController : KoinComponent {
+
+    private val itemRepository by inject<ItemRepository>()
 
     fun getItem(ctx: Context) {
         ctx.pathParam("id").toInt().let {
@@ -14,6 +20,12 @@ class ItemController {
             }
             ctx.status(404)
         }
+    }
+
+    fun addItem(ctx: Context) {
+        ctx.validatedBody<Item>()
+        itemRepository.create(ctx.body().toJsonObject(Item::class.java))
+        return
     }
 
     fun getItem(id: Int) : Item? {
--- src/main/kotlin/org/kat/kotlinwebstack/common/KotlinWebStackModule.kt
+++ src/main/kotlin/org/kat/kotlinwebstack/common/KotlinWebStackModule.kt
@@ -4,6 +4,7 @@ import org.kat.kotlinwebstack.application.web.auth.AuthController
 import org.kat.kotlinwebstack.application.web.item.ItemController
 import org.kat.kotlinwebstack.application.web.message.MessageController
 import org.kat.kotlinwebstack.resources.JwtService
+import org.kat.kotlinwebstack.resources.persistence.ItemRepository
 import org.koin.dsl.module.module
 
 val javalinModule = module {
@@ -11,5 +12,7 @@ val javalinModule = module {
     single { ItemController() }
     single { AuthController() }
     single { MessageController() }
+
+    single { ItemRepository() }
     single { JwtService() }
 }
